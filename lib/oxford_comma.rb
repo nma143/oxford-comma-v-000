d85fb9925b86bc21e8050eaa5963a15ab980f237
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-03-15 15:49:28 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
 
+  if(array.length > 1)
+    array[-1] = "and " + array.last
+  end
+  if (array.length==2)
+    return array[0]+" "+ array[1]
+  else
+    return array.join(", ")
+  end
+
 end
